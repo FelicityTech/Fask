621033192a506c536d91ea30e72630d48f65bae9
Solomon Eniola Adegoke <souceking@gmail.com>
2023-03-11 15:33:39 +0100

    ignore

--- flaskblog.py
+++ flaskblog.py
@@ -7,19 +7,21 @@ posts = [
         'title': 'Blog Post 1',
         'content': 'First post content',
         'date_posted': 'April 20, 2018',
-},
-{
+    },
+    {
         'author': 'Solomon Damilare',
         'title': 'Blog Post 2',
         'content': 'Second post content',
         'date_posted': 'April 21, 2018'
-}
+    }
 ]
+
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
+
 
 @app.route("/about")
 def about():
